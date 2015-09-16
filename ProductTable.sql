2b117e253c1aeda58535cf418903bc84f2b409d9
rhgraham <rhgraham1130@gmail.com>
2015-09-15 20:37:34 -0500

    Added more column declarations and COPY statement
    NOTE: csv file not currently ready for copying (needs uuid's, has null
    data, etc). Also, file path in COPY statement may vary from one machine
    to another.

--- ProductTable.sql
+++ ProductTable.sql
@@ -1,20 +1,34 @@
--- DROP TABLE product;
+﻿-- DROP TABLE product;
 
 CREATE TABLE product
 (
   id uuid NOT NULL,
-  lookupcode character varying(255) NOT NULL DEFAULT ''::character varying,
-  count integer NOT NULL DEFAULT 0,
+  lookupcode character varying(255) NOT NULL DEFAULT '???'::character varying,
+  price real NOT NULL DEFAULT -1.00,
+  itemtype integer NOT NULL DEFAULT -1,
+  cost real NOT NULL DEFAULT -1.00,
+  quantity integer NOT NULL DEFAULT -1,
+  reorderpoint integer NOT NULL DEFAULT 0,
+  restocklevel integer NOT NULL DEFAULT 0,
+  parentitem integer NOT NULL DEFAULT 0,
+  extendeddescription text NOT NULL DEFAULT 'None',
+  active integer NOT NULL DEFAULT 1,
+  msrp integer NOT NULL DEFAULT 1,
   createdon timestamp without time zone NOT NULL DEFAULT now(),
   CONSTRAINT product_pkey PRIMARY KEY (id)
 )
 WITH (
   OIDS=FALSE
 );
+
 ALTER TABLE product
   OWNER TO npcorg;
 
 -- DROP INDEX ix_product_lookupcode;
+
+COPY product
+  FROM 'C:\Program Files\PostgreSQL\9.4\NPC_data.csv'
+  WITH (FORMAT csv, ENCODING LATIN1);
 
 CREATE INDEX ix_product_lookupcode
   ON product
