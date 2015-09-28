7aa7b8e8bb0ac9bc5c99efc231874cf59b382a04
nhardcas <nhardcas@uark.edu>
2015-09-28 09:39:00 -0500

    Initial Transaction Entry Table commit
    Added the initial version of the Transaction Entry database table

---
+++ TransactionEntryTable.sql
@@ -0,0 +1,18 @@
+﻿-- Table: "transactionentry table"
+
+-- DROP TABLE "transactionentry table";
+
+CREATE TABLE "transactionentry table"
+(
+  "Transaction ID" bigint,
+  "Product ID" bigint,
+  "Record ID" bigint NOT NULL,
+  "Price" numeric,
+  "Quantity" integer,
+  CONSTRAINT "PK_recordID" PRIMARY KEY ("Record ID")
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE "transactionentry table"
+  OWNER TO postgres;
