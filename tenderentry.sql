fe16e93f92271d18354a0c7b7b4dbc2279b1104d
rhgraham <rhgraham1130@gmail.com>
2015-10-01 12:10:27 -0500

    Fixed a couple of spelling errors

--- tenderentry.sql
+++ tenderentry.sql
@@ -1,16 +1,16 @@
--- Table: tenderentry
+﻿-- Table: tenderentry
 
 -- DROP TABLE tenderentry;
 
 CREATE TABLE tenderentry
 (
   id uuid NOT NULL,
-  transaction_id integar NOT NULL,
+  transaction_id integer NOT NULL,
   tender_type text NOT NULL DEFAULT 'None',
-  amount Double Percision NOT NULL DEFAULT -1.00,
+  amount Double Precision NOT NULL DEFAULT -1.00,
   createdon timestamp without time zone NOT NULL DEFAULT now(),
-  CONSTRAINT tenderenty_pkey PRIMARY KEY (id),
-  CONSTRAINT tenderenty_fkey FOREIGN KEY (transaction_id)
+  CONSTRAINT id_pkey PRIMARY KEY (id),
+  CONSTRAINT transactionID_fkey FOREIGN KEY (transaction_id)
   REFERENCES transactionentry("Transaction ID")
 )
 WITH
