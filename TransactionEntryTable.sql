a9bf4e246c690b3d09aef362da365c9dade13f36
rhgraham <rhgraham1130@gmail.com>
2015-10-01 11:48:15 -0500

    Changed data types around and added foreign keys
    "integer" is the standard for everything except absolutely huge numbers,
    and "double precision" is best for a price, in my opinion.
    
    NOTE: executing this will cause errors currently, as we do not yet have
    a transaction table

--- TransactionEntryTable.sql
+++ TransactionEntryTable.sql
@@ -4,12 +4,14 @@
 
 CREATE TABLE "transactionentry"
 (
-  "Transaction ID" bigint,
-  "Product ID" bigint,
-  "Record ID" bigint NOT NULL,
-  "Price" numeric,
-  "Quantity" integer,
-  CONSTRAINT "PK_recordID" PRIMARY KEY ("Record ID")
+  "Transaction ID" integer NOT NULL,
+  "Product ID" uuid NOT NULL,
+  "Record ID" integer NOT NULL,
+  "Price" double precision NOT NULL,
+  "Quantity" integer NOT NULL,
+  CONSTRAINT "PK_recordID" PRIMARY KEY ("Record ID"),
+  CONSTRAINT "FK_productID" FOREIGN KEY ("Product ID") REFERENCES product ("id"),
+  CONSTRAINT "FK_transactionID" FOREIGN KEY ("Transaction ID") REFERENCES transaction ("Transaction ID")
 )
 WITH (
   OIDS=FALSE
