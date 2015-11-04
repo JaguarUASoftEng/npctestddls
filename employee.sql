453c91740523cb2d54373855db964a66addccdab
rhgraham <rhgraham1130@gmail.com>
2015-11-04 17:03:56 -0600

    Changed a couple types, added manager FK
    Record IDs should always be of type uuid
    
    Active should be a boolean (unless there are more than 2 possible
    states?)
    
    Implemented same-table FK

--- employee.sql
+++ employee.sql
@@ -4,19 +4,18 @@
 
 CREATE TABLE employee
 (
-  record_id integer NOT NULL,
+  record_id uuid NOT NULL,
   first_name character varying(30),
   last_name character varying(30),
   employee_id integer,
-  active character varying(5),
+  active boolean,
   classification character varying(30),
-  manager character varying(30),
+  manager uuid,
   password character varying(30),
   createdon timestamp without time zone NOT NULL DEFAULT now(),
   CONSTRAINT employee_pkey PRIMARY KEY (record_id),
   CONSTRAINT employee_classification_check CHECK (classification::text = 'general manager'::text OR classification::text = 'shift manager'::text OR classification::text = 'cashier'::text)
---Not sure how to make manager a foreign key
---that references a record on its own table
+  CONSTRAINT manager_fkey FOREIGN KEY (manager) REFERENCES employee (record_id)
 )
 WITH (
   OIDS=FALSE
